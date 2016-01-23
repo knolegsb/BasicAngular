9a6f36e65d268ecd8bc221067a0b54e4ae7da905
knolegsb <seon2you@hotmail.com>
2016-01-23 01:14:31 -0800

    adding controller

---
+++ BasicAngular/script.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("computerApp", [ngRoute]);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/main', {
+            templateUrl: 'main.html',
+            controller: 'mainCtrl'
+        });
+}]);
+
+//.controller('mainCtrl')
