56e222e27606dec9b7a2f94649079ccdd5a73e54
knolegsb <seon2you@hotmail.com>
2016-01-23 13:23:24 -0800

    data fetch

--- BasicAngular/script.js
+++ BasicAngular/script.js
@@ -6,9 +6,32 @@
             templateUrl: 'main.html',
             controller: 'mainCtrl'
         }).
+        when('/about', {
+            templateUrl: 'about.html',
+            controller: 'aboutCtrl'
+        }).
+        when('/services', {
+            templateUrl: 'services.html',
+            controller: 'servicesCtrl'
+        }).
+        when('/contact', {
+            templateUrl: 'contact.html',
+            controller: 'contactCtrl'
+        }).
         otherwise({ redirectTo: '/main' });
 }])
 
 .controller('mainCtrl', ['$scope', function ($scope) {
+    $scope.person = 'John Doe';
+}])
+.controller('aboutCtrl', ['$scope', function ($scope) {
+    $scope.person = 'John Doe';
+}])
+.controller('servicesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('services.json').then(function(response){
+        $scope.services = response.data;
+    });
+}])
+.controller('contactCtrl', ['$scope', function ($scope) {
     $scope.person = 'John Doe';
 }]);
